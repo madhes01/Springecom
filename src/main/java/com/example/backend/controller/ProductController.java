3e802562abf2d91b29e05d5888b9d7988bcbf745
Madheswaran <83126598+madhes01@users.noreply.github.com>
2026-05-03 07:24:30 +0000

    ss

--- src/main/java/com/example/backend/controller/ProductController.java
+++ src/main/java/com/example/backend/controller/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -22,6 +23,15 @@ public class ProductController {
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getProducts() {
         return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("products/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id) {
+        Product product = productService.getProductById(id);
+        if(product == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(product, HttpStatus.OK);
     }
 
 }
