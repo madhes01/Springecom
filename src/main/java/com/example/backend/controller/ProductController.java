2f185fd5c2066499d6a8c56497f9cba4de01f7d3
Madheswaran <83126598+madhes01@users.noreply.github.com>
2026-05-03 14:00:11 +0000

    last1

--- src/main/java/com/example/backend/controller/ProductController.java
+++ src/main/java/com/example/backend/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,7 +22,7 @@ import com.example.backend.service.ProductService;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "https://fluffy-space-capybara-v9pwprqgwv2p7x-5174.app.github.dev")
+@CrossOrigin(origins = "https://fluffy-space-capybara-v9pwprqgwv2p7x-5173.app.github.dev")
 public class ProductController {
 
     @Autowired
@@ -66,7 +67,7 @@ public class ProductController {
         
     }
 
-    @PutMapping("/products/{id}")
+    @PutMapping("/product/{id}")
     public ResponseEntity<?> updateProduct(@PathVariable int id, @RequestPart Product product, @RequestPart MultipartFile imageFile) {
         Product updatedProduct = null;
         try {
@@ -78,7 +79,7 @@ public class ProductController {
         
     }
 
-    @DeleteMapping("/products/{id}")
+    @DeleteMapping("/product/{id}")
     public ResponseEntity<String> deleteProduct(@PathVariable int id) {
         Product product = productService.getProductById(id);
         if (product != null) {
@@ -88,6 +89,14 @@ public class ProductController {
             return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
         }
     }
+
+
+    @GetMapping("/product/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword) {
+        List<Product> products = productService.searchProducts(keyword);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
 }
 
 
--- src/main/java/com/example/backend/service/ProductService.java
+++ src/main/java/com/example/backend/service/ProductService.java
@@ -38,6 +38,10 @@ public class ProductService {
         productRepo.deleteById(id);
     }
 
+    public List<Product> searchProducts(String keyword) {
+       return productRepo.searchProducts(keyword);
+    }
+
     
 
 
