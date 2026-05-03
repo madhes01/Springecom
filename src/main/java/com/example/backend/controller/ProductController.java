3a9a68ce452a07086bf674ae9ff8b867e27541b9
Madheswaran <83126598+madhes01@users.noreply.github.com>
2026-05-03 06:59:35 +0000

    s

--- src/main/java/com/example/backend/controller/ProductController.java
+++ src/main/java/com/example/backend/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.example.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +20,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public ResponseEntityList<Product> getProducts() {
+    public ResponseEntity<List<Product>> getProducts() {
         return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.ACCEPTED);
     }
 
